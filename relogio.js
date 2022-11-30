f5c4b7718cee3fead3b7e7eb889d003291224f04
mat1357913 <106184010+mat1357913@users.noreply.github.com>
2022-11-30 16:53:41 -0300

    Update relogio.js

--- relogio.js
+++ relogio.js
@@ -1,1 +1,17 @@
 
+function clock(){
+    let d = new Date();
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+    let hora = h + ":" + m + ":" + s;
+      
+    if (h < 10){
+        h + "0" + m + ":" + s;
+    }
+    if (m < 10){
+        h + "0" + m + ":" + s;
+    }
+    if (s < 10)
+    h + ":" + m + ":0" + s;
+}
